1c0b95ef1b056e9f94d7b11e613fdbfaa0832ff1
Agustin Escudero <agustinescuderoweb@gmail.com>
2026-01-22 20:01:49 -0300

    nuevo commit

--- app/api/insights/route.js
+++ app/api/insights/route.js
@@ -1,9 +1,9 @@
-import { createClient } from "@supabase/supabase-js";
+import { supabaseServer } from "@/lib/supabaseServer";
 
 // ------------------ SUPABASE CLIENT ------------------
 const supabase = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_ROLE_KEY
+  process.env.SUPABASE_UR,
+  process.env.SUPABASE_ANON_KEY
 );
 
 // ------------------ GET (Leer datos) ------------------
